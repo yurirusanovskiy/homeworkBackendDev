4447cd8637dfd15798fd42a2d0960b043e49f103
yuriy <iurie.rus@rambler.ru>
2024-09-18 10:11:46 +0300

    Refactor fetchData function: split into fetchAllProducts and fetchProductById

--- HW_17_09_2024/server.js
+++ HW_17_09_2024/server.js
@@ -1,15 +1,22 @@
 const http = require("http");
 const PORT = 3000;
 
-async function fetchData(id = "") {
+// Get all products
+async function fetchAllProducts() {
   try {
-    if (id === "") {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching all products:', error);
+    return { error: 'Error fetching all products' };
+  }
+}
 
-      const response = await fetch("https://dummyjson.com/products");
-      const data = await response.json();
-      return data;
-    } else if (!isNaN(id)) {
-
+// Get product by ID
+async function fetchProductById(id) {
+  try {
+    if (!isNaN(Number(id))) {
       const response = await fetch(`https://dummyjson.com/products/${id}`);
       if (response.status === 404) {
         return { error: `Product with id ${id} not found` };
@@ -20,30 +27,44 @@ async function fetchData(id = "") {
       return { error: 'Invalid ID format' };
     }
   } catch (error) {
-    console.error('Error fetching data:', error);
-    return { error: 'Error fetching data' };
+    console.error('Error fetching product by ID:', error);
+    return { error: 'Error fetching product by ID' };
   }
 }
 
+// Create Server
 const server = http.createServer(async (req, res) => {
-  console.log("Server request");
-  console.log(req.url, req.method);
+  console.log("Server request received.");
+  console.log(`URL: ${req.url}, Method: ${req.method}`);
 
   res.setHeader('Content-Type', 'application/json');
 
   const urlParts = req.url.split('/');
-  const id = urlParts[1];
+  const id = urlParts[1] || "";
 
-  const data = await fetchData(id);
+  let data;
+
+  if (id === "") {
+    data = await fetchAllProducts();
+  } else {
+    data = await fetchProductById(id);
+  }
+
+  res.statusCode = 200;
 
   if (data.error) {
-    res.statusCode = id === "" ? 500 : 400;
+    res.statusCode = 400;
     if (data.error.includes('not found')) res.statusCode = 404;
   }
 
   res.end(JSON.stringify(data));
 });
 
+// Start Server
 server.listen(PORT, '127.0.0.1', (err) => {
-  err ? console.log(err) : console.log(`Listening on port ${PORT}`);
+  if (err) {
+    console.log('Error starting server:', err);
+  } else {
+    console.log(`Server is listening on port ${PORT}`);
+  }
 });
